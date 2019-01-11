b39387786ddbe235b8b3ae1886928d326b174464
Arturo Poumian <apoumians@gmail.com>
2019-01-11 18:13:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
+<<<<<<< HEAD
   return [element, ...array];
 }
 
@@ -38,5 +39,53 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length -1);
+=======
+  const listMe = [1];
+  return ['foo', ...listMe];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  const listMe = [1];
+  listMe.unshift('foo');
+  return listMe;
+}
+    
+function addElementToEndOfArray(array, element) {
+  const listMe = [1];
+  return [...listMe, 'foo'];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  const listMe = [1];
+  listMe.push('foo');
+  return listMe;
+}
+
+function accessElementInArray(array, index) {
+  var myCount = [1, 2, 3];
+  return myCount[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  const myCount = [1, 2, 3];
+  myCount.shift();
+  return myCount;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  const myCount = [1, 2, 3];
+  return myCount.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  const myCount = [1, 2, 3];
+  myCount.pop();
+  return myCount;
+}
+
+function removeElementFromEndOfArray(array) {
+  const myCount = [1, 2, 3];
+  return myCount.slice(0, myCount.length -1);
+>>>>>>> 24e389c5425495a521ad0486ca3b3480f1a51c12
 }
 
